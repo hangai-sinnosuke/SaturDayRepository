6c4fd2e16c3bcfb84990ea6cfad58f5e8ab4139c
hangai-sinnosuke <shinn7956@gmail.com>
2026-07-04 12:30:00 +0900

    Background実装

---
+++ 20260704/Map.h
@@ -0,0 +1,15 @@
+#pragma once
+class Map
+{
+private:
+	//The x position of the object int the world
+	float worldX;
+	float worldY;
+public:
+	//コンストラクタ
+	Map(float worldX, float worldY);
+	//描画
+	void Draw(float cameraX);
+
+};
+
